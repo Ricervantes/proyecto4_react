f02c1922e42119c2557c65b081031e3b1f23a2e3
Ricervantes <racervantes.dwfsc9@ucamp.utel.edu.mx>
2022-11-03 16:42:48 -0600

    navbar se cambia a elementos de a a NavLink y Href a to

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,31 +6,31 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <NavLink className="navbar-brand"to="/">Navbar</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Homepage</a>
+                                <NavLink className="nav-link active" aria-current="page"to="/Homepage">Homepage</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">About</a>
+                                <NavLink className="nav-link"to="/About">About</NavLink>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <NavLink className="nav-link dropdown-toggle"to="/Menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Menu
-                                </a>
+                                </NavLink>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="/">Carne</a></li>
-                                    <li><a className="dropdown-item" href="/">Pollo</a></li>
+                                    <li><NavLink className="dropdown-item"to="/Menu">Carne</NavLink></li>
+                                    <li><NavLink className="dropdown-item"to="/Menu">Pollo</NavLink></li>
                                     <li><hr className="dropdown-divider" /></li>
-                                    <li><a className="dropdown-item" href="/">Mariscos</a></li>
+                                    <li><NavLink className="dropdown-item"to="/Menu">Mariscos</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
+                                <NavLink className="nav-link"to="/Contacto">Reservacion</NavLink>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-   <>
+    <>
     <header className="w3-display-container w3-content w3-wide" style={{ maxWidth: 1600, minWidth: 500 }} id="home">
           <img className="w3-image" src="https://www.w3schools.com/w3images/hamburger.jpg" alt="Hamburger Catering" width={1600} height={800} />
           <div className="w3-display-bottomleft w3-padding-large w3-opacity">
@@ -10,7 +10,7 @@ const Homepage = () => {
           </div>
         </header>
    </>
-  )
+  );
 }
 
 export default Homepage
